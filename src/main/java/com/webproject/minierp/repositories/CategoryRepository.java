cfead37adb1b80f893865a1cc6f971a79d421b82
Mohamed-Abbes <mohamedabbes001@gmail.com>
2025-03-03 15:10:03 +0100

    Added repositories. Closes #8

---
+++ src/main/java/com/webproject/minierp/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.webproject.minierp.repositories;
+
+import com.webproject.minierp.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+
+
+
+}
---
+++ src/main/java/com/webproject/minierp/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.webproject.minierp.repositories;
+
+import com.webproject.minierp.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
+
+}
